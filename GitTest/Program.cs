50ab302ae9d9392673aece101f3a537a5857ad21
david <david@DESKTOP-5HNT3PF>
2021-03-31 12:38:47 +0200

    Add project files.

---
+++ GitTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("kreiranje projekta i repoa");
+
+        }
+    }
+}
